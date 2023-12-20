5fc0bed9863d4bde632bc4d3e59f162e036c4e58
shubham dolas <shubhamdolas1210@gmail.com>
2023-12-20 11:23:36 +0530

    navanar uddated

--- src/App.js
+++ src/App.js
@@ -4,34 +4,8 @@ import Navbar from "./Component/Navbar";
 
 function App() {
   return (
-<<<<<<< HEAD
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-<<<<<<< HEAD
-         HELLO WORLD
-=======
-          Edit <code>src/App.js</code> and save to reload.
->>>>>>> 3ea52f3 (Initialize project using Create React App)
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-<<<<<<< HEAD
-          Learning react
-=======
-          Learn React
->>>>>>> 3ea52f3 (Initialize project using Create React App)
-        </a>
-      </header>
-=======
-    <div>
+     <div>
       <Navbar />
->>>>>>> a4a69b1 (Navbar Updated)
     </div>
   );
 }
